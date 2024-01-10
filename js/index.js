9edbf8f77ef0c4c8120122f1fcb1f6b48fe889be
Antonio Trujillo <antoniotrujillodiaz@gmail.com>
2024-01-10 23:18:21 +0000

    añadido js ecotips y flexbox grafica y ecotips

--- js/index.js
+++ js/index.js
@@ -88,3 +88,31 @@ function obtenerDatosHoraActual() {
   }
   
   obtenerDatosHoraActual();
+
+
+  //Funcion ecoConsejos
+
+  //Array con consejos. Lo suyo es enlazar un JSON con todos los EcoConsejos.
+  const ecoConsejos = [
+    "Apaga las luces cuando no las necesites.",
+    "Utiliza bombillas LED de bajo consumo.",
+    "Desenchufa los electrodomésticos cuando no los uses.",
+    "Usa la lavadora y el lavavajillas solo cuando estén llenos.",
+    "Aprovecha la luz natural tanto como sea posible.",
+    "Mantén el termostato a una temperatura razonable.",
+    "Usa cortinas y persianas para mantener la casa fresca en verano y cálida en invierno.",
+    "Cambia los filtros de aire regularmente.",
+    "Usa electrodomésticos eficientes.",
+    "Instala paneles solares para generar tu propia energía."
+  ];
+  
+  //Al parrafo del aside le inyectamos el contenido de la const ecoConsejos
+  let index = 0;
+  const aside = document.querySelector("aside>p");
+  aside.textContent = ecoConsejos[index];
+  
+  //Cada 2 min se muestra un consejo diferente
+  setInterval(() => {
+    index = (index + 1) % ecoConsejos.length;
+    aside.textContent = ecoConsejos[index];
+  }, 2 * 60 * 1000);
