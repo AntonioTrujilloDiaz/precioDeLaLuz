5e567e4def07341583d56b28d2f0fdb1d9916416
Daniel Moreno <morenoacedo@gmail.com>
2024-01-13 14:18:08 +0100

    mejorado ecoTips y estilos en cajas

--- js/ecoTips.js
+++ js/ecoTips.js
@@ -38,15 +38,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
        // Estructura de datos para los ecotips
        const textosEcoTips = {
-        1: "...las bombillas LED son una excelente opción para reemplazar las bombillas halógenas. Aunque son un poco más caras que las bombillas convencionales, pueden ayudarte a ahorrar hasta un 80% en la factura de la luz.",
+        1: "... las bombillas LED son una excelente opción. Te ayudan a ahorrar hasta un 80% en la factura.",
         
-        2: "...aprovechar la luz natural abriendo cortinas y persianas, durante las horas del día de más luz, es una forma efectiva de ahorrar energía, reduciendo el uso de luces artificiales y ahorrando electricidad.",
+        2: "... aprovechar la luz natural que entra en casa es una forma efectiva de ahorrar energía.",
 
-        3:"...el modo 'stand by' continúa consumiendo energía, que por poco que sea, se suma al de otros aparatos eléctricos. Puedes evitarlos desenchufándolos o utilizando regletas con interruptor.",
+        3:"... el modo 'stand by' continúa consumiendo energía. Utiliza regletas con interruptor.",
 
-        4:"...que puedes hacer un uso eficiente de la energía programando tus electrodomésticos para determinadas horas del día aprovechando la información que te brindamos aquí.",
+        4:"... programando tus electrodomésticos para determinadas horas del día podrás ahorrar en tu factura.",
 
-        5:"...el consejo más sencillo para ahorrar energía es un uso sensato de las luces y demás aparatos eléctricos de tu hogar. Apagar las luces cuando no las utilizas supondrá un ahorro para tu bolsillo.",
+        5:"... apagar las luces cuando no las utilizas supone un ahorro para tu bolsillo.",
 
         // Podemos añadir más textos aquí manteniendo el formato.
       };
